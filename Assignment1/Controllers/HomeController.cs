ef877ae88bf7ea917c1986cc011fea76df4fa9a7
Tim Weinitz <weinitz.tim@gmail.com>
2021-11-11 12:21:00 +0100

    A controller with at least three views.

--- Assignment1/Controllers/HomeController.cs
+++ Assignment1/Controllers/HomeController.cs
@@ -8,6 +8,18 @@ namespace Assignment1.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }        
+        public IActionResult Projects()
+        {
+            return View();
+        }
     
     }
 }
